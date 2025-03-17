6a8b27ab8031b2fb7e2a006c570979faeca89105
Norbert Koziana <koziana11norbert@gmail.com>
2025-03-17 20:50:08 +0100

    Add postgress and initial oauth2 config

---
+++ src/main/java/com/msn/MSNcars/auth/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.msn.MSNcars.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(request ->
+                        request
+                                .requestMatchers(HttpMethod.GET, "/api/products", "listings/{id}/images")
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated()
+                )
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .cors(CorsConfigurer::disable)
+                .csrf(CsrfConfigurer::disable)
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()));
+        return http.build();
+    }
+}
