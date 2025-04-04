8fe98dfcc0ce3ef3f86dd451455398fc152f944f
Norbert Koziana <koziana11norbert@gmail.com>
2025-04-04 11:12:41 +0200

    Added mapping between keycloak and spring roles

--- src/main/java/com/msn/msncars/TestController.java
+++ src/main/java/com/msn/msncars/TestController.java
@@ -1,8 +1,13 @@
 package com.msn.msncars;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/*
+    Demo controller to test accessing endpoints based on user role
+*/
 @RestController
 public class TestController {
 
@@ -12,7 +17,41 @@ public class TestController {
     }
 
     @GetMapping("/secure")
-    public String secureEndpoint(){
-        return "Hello from secure endpoint";
+    public String secureEndpoint(@AuthenticationPrincipal Jwt authenticationPrincipal){
+        return String.format(
+                "Hello %s with userId: %s, from secure endpoint",
+                authenticationPrincipal.getClaimAsString("preferred_username"),
+                authenticationPrincipal.getClaimAsString("sub")
+        );
+    }
+
+    /*
+        Credentials for sample account with user permissions:
+        login: user
+        password: user
+     */
+    @GetMapping("/user")
+    public String userEndpoint(){
+        return "Hello from user endpoint";
+    }
+
+    /*
+        Credentials for sample account with company permissions:
+        login: company
+        password: company
+     */
+    @GetMapping("/company")
+    public String companyEndpoint(){
+        return "Hello from company endpoint";
+    }
+
+    /*
+        Credentials for sample account with admin permissions:
+        login: admin
+        password: admin
+    */
+    @GetMapping("/admin")
+    public String adminEndpoint(){
+        return "Hello from admin endpoint";
     }
 }
