ac822a7290bf55246fdcd43ebbd977f714c7af09
Blizni13 <szymongryz@interia.pl>
2025-04-29 16:39:50 +0200

    deleteUser method added

--- src/main/java/com/msn/msncars/user/UserService.java
+++ src/main/java/com/msn/msncars/user/UserService.java
@@ -1,14 +1,18 @@
 package com.msn.msncars.user;
 
 import com.msn.msncars.auth.keycloak.KeycloakConfig;
+import jakarta.ws.rs.core.Response;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.representations.idm.UserRepresentation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     private final Keycloak keycloakAPI;
     private final KeycloakConfig keycloakConfig;
+    private final Logger logger = LoggerFactory.getLogger(UserService.class);
 
     public UserService(Keycloak keycloakAPI, KeycloakConfig keycloakConfig) {
         this.keycloakAPI = keycloakAPI;
@@ -17,5 +21,12 @@ public class UserService {
 
     public UserRepresentation getUserRepresentationById(String userId) {
         return keycloakAPI.realm(keycloakConfig.getRealm()).users().get(userId).toRepresentation();
+    }
+
+    public void deleteUser(String userId) {
+        try(Response response = keycloakAPI.realm(keycloakConfig.getRealm()).users().delete(userId)) {
+            if (response.getStatus() != Response.Status.NO_CONTENT.getStatusCode())
+                logger.warn("Failed to delete user {}. Status: {}, {}", userId, response.getStatus(), response.getStatusInfo().getReasonPhrase());
+        }
     }
 }
