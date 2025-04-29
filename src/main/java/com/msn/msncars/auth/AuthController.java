f7df879eb76cd7916f4da24706152debf137904d
Blizni13 <szymongryz@interia.pl>
2025-04-29 16:41:28 +0200

    company and user registration method's added

--- src/main/java/com/msn/msncars/auth/AuthController.java
+++ src/main/java/com/msn/msncars/auth/AuthController.java
@@ -1,6 +1,8 @@
 package com.msn.msncars.auth;
 
+import com.msn.msncars.auth.exception.RegistrationException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,8 +16,20 @@ public class AuthController {
         this.authService = authService;
     }
 
-    @PostMapping("/auth/register")
-    public ResponseEntity<String> register(@RequestBody RegisterRequest registerRequest){
-        return authService.registerUser(registerRequest);
+    @PostMapping("/auth/user/register")
+    public ResponseEntity<String> registerUser(@RequestBody UserRegistrationRequest userRegistrationRequest) {
+        String accountId = authService.registerUserAndAssignRole(userRegistrationRequest, AccountRole.USER);
+        return ResponseEntity.ok(accountId);
+    }
+
+    @PostMapping("/auth/company/register")
+    public ResponseEntity<CompanyRegistrationResponse> registerCompany(@RequestBody CompanyRegistrationRequest companyRegistrationRequest) {
+        CompanyRegistrationResponse companyRegistrationResponse = authService.registerCompany(companyRegistrationRequest);
+        return ResponseEntity.ok(companyRegistrationResponse);
+    }
+
+    @ExceptionHandler(RegistrationException.class)
+    public ResponseEntity<String> handleRegistrationException(RegistrationException re) {
+        return ResponseEntity.status(re.getStatus()).body(re.getMessage());
     }
 }
