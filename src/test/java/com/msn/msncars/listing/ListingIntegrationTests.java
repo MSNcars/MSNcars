dae50721c972394c76cbfbab1ed605177955bbf0
Mateusz Wołowiec <mwwyoghurt@gmail.com>
2025-04-30 10:54:18 +0200

    updateListing_ShouldReturn400Code_AndAccordingMessage_WhenListingIsRevoked test added

--- src/test/java/com/msn/msncars/listing/ListingIntegrationTests.java
+++ src/test/java/com/msn/msncars/listing/ListingIntegrationTests.java
@@ -9,13 +9,11 @@ import com.msn.msncars.car.model.ModelRepository;
 import com.msn.msncars.company.Company;
 import com.msn.msncars.company.CompanyRepository;
 import com.msn.msncars.listing.DTO.ListingRequest;
-import com.msn.msncars.listing.exception.ListingNotFoundException;
 import com.zaxxer.hikari.HikariDataSource;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -36,12 +34,8 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 
 import javax.sql.DataSource;
 import java.math.BigDecimal;
-import java.time.LocalDate;
 import java.time.ZonedDateTime;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 
 import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -90,9 +84,6 @@ public class ListingIntegrationTests {
 
     @Autowired
     FeatureRepository featureRepository;
-
-    @Autowired
-    private DataSource dataSource;
 
     @Autowired
     ObjectMapper objectMapper;
@@ -841,6 +832,93 @@ public class ListingIntegrationTests {
         mockMvc.perform(request)
                 .andExpect(status().isNotFound())
                 .andExpect(content().string("Listing not found with id: 1"));
+    }
+
+    @Test
+    public void updateListing_ShouldReturn400Code_AndAccordingMessage_WhenListingIsRevoked() throws Exception {
+        // given
+
+        Make toyota = new Make(1L, "Toyota");
+
+        Model corolla = new Model(1L, "Corolla", toyota);
+        Model yaris = new Model(2L, "Corolla", toyota);
+
+        Company autoWorld = new Company(null,
+                "1",
+                "Auto World",
+                "123 Main St",
+                "123-456-789",
+                "contact@autoworld.com");
+
+        Feature sunroof = new Feature(null, "Sunroof");
+        Feature navigation = new Feature(null, "Navigation");
+
+        Listing listingInDatabase = new Listing(
+                null,
+                "1",
+                autoWorld,
+                corolla,
+                List.of(sunroof, navigation),
+                ZonedDateTime.now(),
+                ZonedDateTime.now().plusMonths(1),
+                true,
+                new BigDecimal("18000.00"),
+                2020,
+                45000,
+                Fuel.PETROL,
+                CarUsage.USED,
+                CarOperationalStatus.WORKING,
+                CarType.SEDAN,
+                "Well maintained Toyota Corolla with sunroof and nav."
+        );
+
+        makeRepository.save(toyota);
+        modelRepository.save(corolla);
+        modelRepository.save(yaris);
+        companyRepository.save(autoWorld);
+        featureRepository.save(sunroof);
+        featureRepository.save(navigation);
+        Listing savedListing = listingRepository.save(listingInDatabase);
+        Long listingInDatabaseId = savedListing.getId();
+
+        // want to update model and price
+        ListingRequest listingUpdateRequest = new ListingRequest(
+                "1",
+                1L,
+                2L,
+                List.of(sunroof.getId(), navigation.getId()),
+                new BigDecimal("12000.00"),
+                2020,
+                45000,
+                Fuel.PETROL,
+                CarUsage.USED,
+                CarOperationalStatus.WORKING,
+                CarType.SEDAN,
+                "Well maintained Toyota Corolla with sunroof and nav.",
+                ValidityPeriod.Standard
+        );
+
+        String requestJson = objectMapper.writeValueAsString(listingUpdateRequest);
+
+        String userId = "1";
+
+        Jwt jwt = Jwt.withTokenValue("token")
+                .header("alg", "none")
+                .claim("sub", userId)
+                .build();
+
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .put("/listings/" + listingInDatabaseId)
+                .with(jwt().jwt(jwt))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestJson);
+
+        // when & then
+
+        mockMvc.perform(request)
+                .andExpect(status().isBadRequest())
+                .andExpect(content().string("Cannot update revoked listing."));
+
     }
 
     @AfterAll
