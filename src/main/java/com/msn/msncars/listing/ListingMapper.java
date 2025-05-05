2dc0a68dd65d493fdbdf604eaf61d22a2d11cadb
Blizni13 <szymongryz@interia.pl>
2025-05-05 16:00:39 +0200

    mapper updated to convert instant date into zonedDateTime to display it for the user

--- src/main/java/com/msn/msncars/listing/ListingMapper.java
+++ src/main/java/com/msn/msncars/listing/ListingMapper.java
@@ -3,9 +3,21 @@ package com.msn.msncars.listing;
 import com.msn.msncars.listing.DTO.ListingResponse;
 import com.msn.msncars.listing.DTO.ListingRequest;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 @Mapper(componentModel = "spring")
 public interface ListingMapper {
     Listing fromDTO(ListingRequest listingRequest);
-    ListingResponse toDTO(Listing listing);
+    @Mapping(target = "id", source = "listing.id")
+    @Mapping(target = "createdAt", expression = "java(map(listing.getCreatedAt(), userTimeZone))")
+    @Mapping(target = "expiresAt", expression = "java(map(listing.getExpiresAt(), userTimeZone))")
+    ListingResponse toDTO(Listing listing, ZoneId userTimeZone);
+
+    default ZonedDateTime map(Instant instant, ZoneId userTimeZone) {
+        return ZonedDateTime.ofInstant(instant, userTimeZone);
+    }
 }
