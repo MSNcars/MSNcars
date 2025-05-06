cd1ee6b8737b673d8982287fe1e82591443337e0
Blizni13 <szymongryz@interia.pl>
2025-05-06 16:48:47 +0200

    TestController endpoints renamed

--- src/main/java/com/msn/msncars/TestController.java
+++ src/main/java/com/msn/msncars/TestController.java
@@ -3,12 +3,15 @@ package com.msn.msncars;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /*
     Demo controller to test accessing endpoints based on user role
 */
+
 @RestController
+@RequestMapping("/test")
 public class TestController {
 
     @GetMapping("/public")
