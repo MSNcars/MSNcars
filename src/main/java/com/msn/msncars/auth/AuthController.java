82903c385df4440ac8a901b4bd46b7522f57de9b
Blizni13 <szymongryz@interia.pl>
2025-05-18 13:56:39 +0200

    Logging feature added.

--- src/main/java/com/msn/msncars/auth/AuthController.java
+++ src/main/java/com/msn/msncars/auth/AuthController.java
@@ -1,6 +1,8 @@
 package com.msn.msncars.auth;
 
 import com.msn.msncars.auth.exception.RegistrationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,24 +14,40 @@ public class AuthController {
 
     private final AuthService authService;
 
+    private final Logger logger = LoggerFactory.getLogger(AuthController.class);
+
     public AuthController(AuthService authService) {
         this.authService = authService;
     }
 
     @PostMapping("/auth/user/register")
     public ResponseEntity<String> registerUser(@RequestBody UserRegistrationRequest userRegistrationRequest) {
+        logger.info("Received request to register a new user account.");
+
         String accountId = authService.registerUserAndAssignRole(userRegistrationRequest, AccountRole.USER);
+
+        logger.info("User account has been successfully registered, accountId: {}", accountId);
+
         return ResponseEntity.ok(accountId);
     }
 
     @PostMapping("/auth/company/register")
     public ResponseEntity<CompanyRegistrationResponse> registerCompany(@RequestBody CompanyRegistrationRequest companyRegistrationRequest) {
+        logger.info("Received request to register a new company account.");
+
         CompanyRegistrationResponse companyRegistrationResponse = authService.registerCompany(companyRegistrationRequest);
+
+        logger.info("Company has been successfully registered, userId: {}, companyId: {}",
+            companyRegistrationResponse.userId(),
+            companyRegistrationResponse.companyId()
+        );
+
         return ResponseEntity.ok(companyRegistrationResponse);
     }
 
     @ExceptionHandler(RegistrationException.class)
     public ResponseEntity<String> handleRegistrationException(RegistrationException re) {
+        logger.warn("Registration exception occurred, reason: {}", re.getMessage());
         return ResponseEntity.status(re.getStatus()).body(re.getMessage());
     }
 }
--- src/main/java/com/msn/msncars/car/make/MakeServiceImpl.java
+++ src/main/java/com/msn/msncars/car/make/MakeServiceImpl.java
@@ -1,16 +1,19 @@
 package com.msn.msncars.car.make;
 
 import com.msn.msncars.car.exception.MakeNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class MakeServiceImpl implements MakeService{
 
     private final MakeRepository makeRepository;
     private final MakeMapper makeMapper;
+
+    private final Logger logger = LoggerFactory.getLogger(MakeServiceImpl.class);
 
     public MakeServiceImpl(MakeRepository makeRepository, MakeMapper makeMapper) {
         this.makeRepository = makeRepository;
@@ -19,16 +22,34 @@ public class MakeServiceImpl implements MakeService{
 
     @Override
     public List<MakeSlimDTO> getAllMakes() {
-        return makeRepository.findAll().stream().map(makeMapper::toSlimDTO).toList();
+        logger.debug("Entering getAllMakes method.");
+
+        List<MakeSlimDTO> makeSlimDTOS = makeRepository.findAll().stream().map(makeMapper::toSlimDTO).toList();
+
+        logger.debug("Makes ({}) fetched from database and mapped to dtos.", makeSlimDTOS.size());
+
+        return makeSlimDTOS;
     }
 
     @Override
     public List<MakeDTO> getAllMakesWithAssociatedModels() {
-        return makeRepository.findAll().stream().map(makeMapper::toDTO).toList();
+        logger.debug("Entering getAllMakesWithAssociatedModels method.");
+
+        List<MakeDTO> makeDTOS = makeRepository.findAll().stream().map(makeMapper::toDTO).toList();
+
+        logger.debug("Makes ({}) with associated models fetched from database.", makeDTOS.size());
+
+        return makeDTOS;
     }
     @Override
     public MakeDTO getMakeInformation(String makeName) {
-        return makeRepository.findByName(makeName).map(makeMapper::toDTO)
+        logger.debug("Entering getMakeInformation method for make: {}.", makeName);
+
+        MakeDTO makeDTO = makeRepository.findByName(makeName).map(makeMapper::toDTO)
                 .orElseThrow(() -> new MakeNotFoundException(String.format("Make with name %s was not found", makeName)));
+
+        logger.debug("Make successfully fetched from database and mapped.");
+
+        return makeDTO;
     }
 }
