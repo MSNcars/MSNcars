3e46cd6476d7b213f1d2eaae11dd8bdc3dcd8a00
Blizni13 <szymongryz@interia.pl>
2025-06-01 12:18:42 +0200

    login endpoint created for swagger documentation, global 401 response handled

--- src/main/java/com/msn/msncars/config/SwaggerConfig.java
+++ src/main/java/com/msn/msncars/config/SwaggerConfig.java
@@ -2,7 +2,16 @@ package com.msn.msncars.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Operation;
+import io.swagger.v3.oas.models.PathItem;
+import io.swagger.v3.oas.models.examples.Example;
+import io.swagger.v3.oas.models.media.*;
+import io.swagger.v3.oas.models.parameters.RequestBody;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.responses.ApiResponses;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,6 +20,7 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
+                .addServersItem(new Server().url("http://localhost:80"))
                 .components(new Components().addSecuritySchemes("bearerAuth",
                         new SecurityScheme()
                                 .name("Authorization")
@@ -19,5 +29,52 @@ public class SwaggerConfig {
                                 .bearerFormat("JWT")
                                 .in(SecurityScheme.In.HEADER)
                 ));
+    }
+
+    @Bean
+    public OpenApiCustomizer externalLoginEndpoint() {
+        return openApi ->
+            openApi.path("/auth/login",
+                    new PathItem().post(
+                            new Operation()
+                                    .summary("Login")
+                                    .description("Send a request to obtain a JWT token. Required fields: username, password, grant_type, client_id.")
+                                    .addTagsItem("Authentication")
+                                    .requestBody(new RequestBody()
+                                            .required(true)
+                                            .description("Keycloak login request")
+                                            .content(
+                                                    new Content().addMediaType("application/x-www-form-urlencoded",
+                                                        new MediaType()
+                                                                .addEncoding("grant_type", new Encoding())
+                                                                .addEncoding("client_id", new Encoding())
+                                                                .addEncoding("username", new Encoding())
+                                                                .addEncoding("password", new Encoding())
+                                                                .schema(new Schema<>().type("object")
+                                                                        .addProperty("grant_type", new StringSchema()._default("password"))
+                                                                        .addProperty("client_id", new StringSchema()._default("MSNcars"))
+                                                                        .addProperty("username", new StringSchema().example("user"))
+                                                                        .addProperty("password", new StringSchema().example("user")))
+                                                    )))
+                                    .responses(new ApiResponses()
+                                            .addApiResponse("200", new ApiResponse().description("Successful login"))
+                                            .addApiResponse("401", new ApiResponse().description("Invalid credentials"))
+                                    )
+                    )
+            );
+    }
+
+    @Bean
+    public OpenApiCustomizer globalResponsesCustomizer() {
+        return openApi -> openApi.getPaths().values().forEach(pathItem -> {
+
+            for (PathItem.HttpMethod method : pathItem.readOperationsMap().keySet()) {
+                Operation operation = pathItem.readOperationsMap().get(method);
+                ApiResponses responses = operation.getResponses();
+
+                responses.addApiResponse("401", new ApiResponse().description("Authentication required: please provide a valid access token."));
+            }
+
+        });
     }
 }
