75bef2019979fec48f2ff52c7b4e53c51d190914
Blizni13 <szymongryz@interia.pl>
2025-06-11 20:31:10 +0200

    replace InvitationDTO with two classes - each depending on side which is reading invitation (company or recipient)

--- src/main/java/com/msn/msncars/company/invitation/InvitationDTO.java
+++
@@ -1,4 +0,0 @@
-package com.msn.msncars.company.invitation;
-
-public record InvitationDTO(String id, Long senderCompanyId, String creationDate, InvitationState invitationState) {
-}
---
+++ src/main/java/com/msn/msncars/company/invitation/InvitationSentByCompanyDTO.java
@@ -0,0 +1,4 @@
+package com.msn.msncars.company.invitation;
+
+public class InvitationSentByCompanyDTO {
+}
